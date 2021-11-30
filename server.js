10cbc962f5a1e51f6fd927d5022b1888e66d85a1
SAM <stackeberry@gmail.com>
2021-11-30 18:52:32 -0500

    commit 5

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/users/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").all(req.params.id);
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
 	res.status(200).json(stmt);
 });
 
@@ -49,7 +49,7 @@ app.get("/app/users/:id", (req, res) => {
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?")
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
-	res.status(405).json({"message": `1 record updated: ID ${req.params.id} (405)`});
+	res.status(200).json({"message": `1 record updated: ID ${req.params.id} (200)`});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
